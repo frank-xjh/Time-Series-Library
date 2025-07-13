e04b38cbb8aa74329728bd981d9703d5451f561a
Frank <34837108+frank-xjh@users.noreply.github.com>
2025-07-13 16:26:13 +0800

    refactor: move to uv

--- run.py
+++ run.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     # GPU
     parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
-    parser.add_argument('--gpu_type', type=str, default='cuda', help='gpu type')  # cuda or mps
+    parser.add_argument('--gpu_type', type=str, default='mps', help='gpu type')  # cuda or mps
     parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
     parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
 
